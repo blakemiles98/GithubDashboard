a88d0c7b541fbf76dadc12ba915b56f16cfaef3b
Blake Miles <88909449+blakemiles98@users.noreply.github.com>
2025-03-09 10:14:21 -0500

    remove files

--- GithubDashboard.UI/github-dashboard/src/services/githubService.ts
+++
@@ -1,9 +0,0 @@
-export const getUserRepos = async (token: string) => {
-    const response = await fetch("https://api.github.com/user/repos", {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: "application/vnd.github.v3+json",
-        },
-    });
-    return response.json();
-};
